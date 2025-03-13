ec2ebddd57a91a4d5511b3337bfd039c84536a78
SaraReginaRuvalcabaM <24150918@aguascalientes.tecnm.mx>
2025-03-13 14:29:34 -0600

    Add files via upload

---
+++ MisClases/ClaseVehiculoDeCarga.py
@@ -0,0 +1,32 @@
+class VehiculoDeCarga:
+    def __init__(self,numero_de_vehiculo=0,tipo="", placas="",capmax=0.0, peso_del_producto=0.0, peso_tara=0.0):
+        self.__numero_de_vehiculo=numero_de_vehiculo
+        self.__tipo=tipo
+        self.__capmax=capmax
+        self.__peso_del_producto=peso_del_producto
+        self.__peso_tara=peso_tara
+    def get_numero_de_vehiculo(self):
+        return self.__numero_de_vehiculo
+    def set_numero_de_vehiculo(self,numero_de_vehiculo):
+        self.__numero_de_vehiculo=numero_de_vehiculo
+    def get_tipo(self):
+        return self.__tipo
+    def set_tipo(self,tipo):
+        self.__tipo=tipo
+    def get_capmax(self):
+        return self.__capmax
+    def set_capmax(self,capmax):
+        self.__capmax=capmax
+    def get_peso_del_producto(self):
+        return self.__peso_del_producto
+    def set_peso_del_producto(self,peso_del_producto):
+        self.__peso_del_producto=peso_del_producto
+    def get_peso_tara(self):
+        return self.__peso_tara
+    def set_peso_tara(self,peso_tara):
+        self.__peso_tara=peso_tara
+    def __str__(self):
+        return f"Vehiculo tiene:\n{self.get_numero_de_vehiculo()}"
+
+
+
